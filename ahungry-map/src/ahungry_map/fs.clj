70b30e5208baf421fd5517fc0e176c76cf740d0d
Matthew Carter <m@ahungry.com>
2019-04-07 01:26:08 -0400

    Rewrite that terrible cpp into clojure

---
+++ ahungry-map/src/ahungry_map/fs.clj
@@ -0,0 +1,17 @@
+(ns ahungry-map.fs
+  (:require [quil.core :as q]
+            [quil.middleware :as m]))
+
+;; Code related to parsing the files on disk.
+
+;; (parse-map-lines "/home/mcarter/src/ahungry-map/res/maps/tutorialb.txt")
+;; Line format is as such:
+;; L 1186.0742, -2175.0840, 3.1260,  1215.0065, -2174.9312, 3.1260,  150, 0, 200
+(defn parse-line [s]
+  (->> (clojure.string/split s #",* +")
+      (zipmap [:t :x1 :x2 :y1 :y2 :z1 :z2 :r :g :b :a :label])))
+
+(defn parse-map-lines [file-name]
+  (->>
+   (clojure.string/split (slurp file-name) #"\r\n")
+   (map parse-line)))
---
+++ ahungry-map/src/ahungry_map/fs_test.clj
@@ -0,0 +1,22 @@
+(ns ahungry-map.fs-test
+  (:require
+   [ahungry-map.fs :as afs]
+   [clojure.test :as t :refer [deftest testing is run-tests]]
+   ))
+
+(deftest parse-line []
+  (testing "Ensure line parse is as expected."
+    (let [parsed (afs/parse-line "L 1186.0742, -2175.0840, 3.1260,  1215.0065, -2174.9312, 3.1260,  150, 0, 200")]
+      (is (= "L" (:t parsed)))
+      (is (= "1186.0742" (:x1 parsed)))
+      (is (= "-2175.0840" (:x2 parsed)))
+      (is (= "3.1260" (:y1 parsed)))
+      (is (= "1215.0065" (:y2 parsed)))
+      (is (= "-2174.9312" (:z1 parsed)))
+      (is (= "3.1260" (:z2 parsed)))
+      (is (= "150" (:r parsed)))
+      (is (= "0" (:g parsed)))
+      (is (= "200" (:b parsed)))
+      )))
+
+(run-tests)
