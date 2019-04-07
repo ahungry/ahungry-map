a9b25737a4906c166b43d6f5a2f5a8b79c6cd870
Matthew Carter <m@ahungry.com>
2019-04-07 01:28:14 -0400

    more format

--- ahungry-map/src/ahungry_map/fs.clj
+++ ahungry-map/src/ahungry_map/fs.clj
@@ -7,7 +7,7 @@
 ;; L 1186.0742, -2175.0840, 3.1260,  1215.0065, -2174.9312, 3.1260,  150, 0, 200
 (defn parse-line [s]
   (->> (clojure.string/split s #",* +")
-      (zipmap [:t :x1 :x2 :y1 :y2 :z1 :z2 :r :g :b :a :label])))
+       (zipmap [:t :x1 :x2 :y1 :y2 :z1 :z2 :r :g :b :a :label])))
 
 (defn parse-map-lines [file-name]
   (->> (clojure.string/split (slurp file-name) #"\r\n")
