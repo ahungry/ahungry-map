3202b06368ee37319c1686e894f044c070e66f6d
Matthew Carter <m@ahungry.com>
2019-04-07 22:40:54 -0400

    Woops, fix map parse

--- ahungry-map/src/ahungry_map/core.clj
+++ ahungry-map/src/ahungry_map/core.clj
@@ -4,7 +4,7 @@
    [quil.core :as q]
    [quil.middleware :as m]))
 
-(def world-map (afs/parse-map-lines "/home/mcarter/src/ahungry-map/res/maps/tutorialb.txt"))
+(def world-map (afs/parse-map-lines "/home/mcarter/src/ahungry-map/res/maps/guildhall.txt"))
 
 (defn setup []
   ; Set frame rate to 1 frames per second.
@@ -13,6 +13,14 @@
   (q/color-mode :hsb)
   ; setup function returns initial state. It contains
   ; circle color and position.
+  (doall (map (fn [{:keys [t x1 x2 y1 y2]}]
+                (when (= "L" t)
+                  (q/line (/ (read-string x1) 1)
+                          (/ (read-string y1) 1)
+                          (/ (read-string x2) 1)
+                          (/ (read-string y2) 1))
+                  (prn "Pts: " x1 y1 x2 y2)))
+              (take 3000 world-map)))
   {:color 0
    :angle 0})
 
@@ -22,27 +30,32 @@
    :angle (+ (:angle state) 0.1)})
 
 (defn draw-state [state]
-  ; Clear the sketch by filling it with light-grey color.
+  ;; Clear the sketch by filling it with light-grey color.
   (q/background 240)
-  ; Set circle color.
+  ;; Set circle color.
   (q/fill (:color state) 255 255)
-  ; Calculate x and y coordinates of the circle.
-  (let [angle (:angle state)
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
-    ; Move origin point to the center of the sketch.
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
 
-      ;; Draw map
-      (doall (map (fn [{:keys [x1 x2 y1 y2]}]
-                 (q/line (read-string x1)
-                         (read-string y1)
-                         (read-string x2)
-                         (read-string y2))) (take 300 world-map)))
+  (q/with-translation [500 500]
+    ;; Draw map
+    (doall (map (fn [{:keys [t x1 x2 y1 y2]}]
+                  (when (= "L" t)
+                    (q/line (/ (read-string x1) 1)
+                            (/ (read-string y1) 1)
+                            (/ (read-string x2) 1)
+                            (/ (read-string y2) 1))))
+                (take 3000 world-map))))
 
-      ;; Draw the circle.
-      (q/ellipse x y 100 100))))
+  ;; Calculate x and y coordinates of the circle.
+  ;; (let [angle (:angle state)
+  ;;       x (* 150 (q/cos angle))
+  ;;       y (* 150 (q/sin angle))]
+  ;;                                       ; Move origin point to the center of the sketch.
+  ;;   (q/with-translation [(/ (q/width) 2)
+  ;;                        (/ (q/height) 2)]
+
+  ;;     ;; Draw the circle.
+  ;;     (q/ellipse x y 100 100)))
+  )
 
 (defn hmm []
   (do (map (fn [{:keys [x1 x2 y1 y2]}]
@@ -54,7 +67,7 @@
 
 (q/defsketch ahungry-map
   :title "You spin my circle right round"
-  :size [500 500]
+  :size [900 900]
   ; setup function called only once, during sketch initialization.
   :setup setup
   ; update-state is called on each iteration before draw-state.
--- ahungry-map/src/ahungry_map/fs.clj
+++ ahungry-map/src/ahungry_map/fs.clj
@@ -5,9 +5,10 @@
 ;; (parse-map-lines "/home/mcarter/src/ahungry-map/res/maps/tutorialb.txt")
 ;; Line format is as such:
 ;; L 1186.0742, -2175.0840, 3.1260,  1215.0065, -2174.9312, 3.1260,  150, 0, 200
+;; L -12.7163, 162.0129, 0.0020,  12.6721, 162.0129, 0.0020,  0, 0, 0
 (defn parse-line [s]
   (->> (clojure.string/split s #",* +")
-       (zipmap [:t :x1 :x2 :y1 :y2 :z1 :z2 :r :g :b :a :label])))
+       (zipmap [:t :x1 :y1 :z1 :x2 :y2 :z2 :r :g :b :a :label])))
 
 (defn parse-map-lines [file-name]
   (->> (clojure.string/split (slurp file-name) #"\r\n")
