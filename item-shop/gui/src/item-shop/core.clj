7fb82e61cd9aef97d7b1b6556e88bfa90a150977
Matthew Carter <m@ahungry.com>
2019-05-14 01:05:38 -0400

    Working query against sqlite db

--- item-shop/gui/src/item-shop/core.clj
+++ item-shop/gui/src/item-shop/core.clj
@@ -1,5 +1,18 @@
+;; https://github.com/cljfx/cljfx
+;; https://clojure.org/guides/deps_and_cli
 (ns item-shop.core
-  (:require [cljfx.api :as fx]))
+  (:require
+   [cljfx.api :as fx]
+   [clojure.java.jdbc :as j]
+   ))
+
+(defn db []
+  {:dbtype "sqlite"
+   :dbname "../item-shop.db"})
+
+(defn test-db []
+  (j/query (db)
+           ["SELECT COUNT(*) FROM eqItems"]))
 
 (defn -main [& args]
   (fx/on-fx-thread
