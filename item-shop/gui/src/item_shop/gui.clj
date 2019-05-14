8ce64a8995551286f4efd6ea3b811244b95121a5
Matthew Carter <m@ahungry.com>
2019-05-14 16:29:39 -0400

    Need to find mouse event next

--- item-shop/gui/src/item_shop/gui.clj
+++ item-shop/gui/src/item_shop/gui.clj
@@ -5,7 +5,47 @@
 (ns item-shop.gui
   (:require
    [cljfx.api :as fx]
+   )
+  (:import
+   [javafx.scene.input KeyCode KeyEvent]
    ))
+
+(def *state* (atom {}))
+
+(defn map-event-handler [event]
+  (prn "got an event!" event)
+  (case (:event/type event)
+    ::set-done (swap! *state* assoc-in [:by-id (:id event) :done] (:fx/event event))))
+
+(defn root [{:keys [by-id typed-text]}]
+  {:fx/type :stage
+   :showing true
+   :title "Cljfx example"
+   :width 300
+   :height 100
+   :scene {:fx/type :scene
+           :root {:fx/type :v-box
+                  :alignment :center
+                  :children
+                  [{:fx/type :label
+                    :text "Hello world"}
+                   {:fx/type :list-view
+                    :cell-factory (fn [i]
+                                    (let [color (format "#%03x" i)]
+                                      {:style {:-fx-background-color color}
+                                       :mouse-clicked {:event/type ::set-done :id i}
+                                       ;; :on-selected-clicked {:event/type ::set-done :id 3}
+                                       :text color}))
+                    :items (range 16r1000)
+                    ;; :items ["one" :two]
+                    }]}}})
+
+(defn xmain [& args]
+  (fx/mount-renderer
+   *state*
+   (fx/create-renderer
+    :middleware (fx/wrap-map-desc assoc :fx/type root)
+    :opts {:fx.opt/map-event-handler map-event-handler})))
 
 (defn main [& args]
   (fx/on-fx-thread
@@ -26,6 +66,6 @@
                                       (let [color (format "#%03x" i)]
                                         {:style {:-fx-background-color color}
                                          :text color}))
-                      :items (range 16r1000)
+                      :items (range 16r100000)
                       ;; :items ["one" :two]
                       }]}}})))
