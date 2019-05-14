2caad0ccaca8911d6c858a3b8c95286405af4714
Matthew Carter <m@ahungry.com>
2019-05-14 01:58:49 -0400

    Nice, very nice

--- item-shop/gui/src/item-shop/gui.clj
+++ item-shop/gui/src/item-shop/gui.clj
@@ -1,6 +1,7 @@
 ;; https://github.com/cljfx/cljfx
 ;; https://clojure.org/guides/deps_and_cli
 ;; https://github.com/clojure/tools.deps.alpha/wiki/Tools
+;; https://github.com/cljfx/cljfx/blob/30a8c71d5bab559a733e4bb19d1412ef1227b0f5/examples/e16_cell_factories.clj
 (ns item-shop.gui
   (:require
    [cljfx.api :as fx]
@@ -17,5 +18,14 @@
      :scene {:fx/type :scene
              :root {:fx/type :v-box
                     :alignment :center
-                    :children [{:fx/type :label
-                                :text "Hello world"}]}}})))
+                    :children
+                    [{:fx/type :label
+                      :text "Hello world"}
+                     {:fx/type :list-view
+                      :cell-factory (fn [i]
+                                      (let [color (format "#%03x" i)]
+                                        {:style {:-fx-backround-color color}
+                                         :text color}))
+                      :items (range 16r1000)
+                      ;; :items ["one" :two]
+                      }]}}})))
