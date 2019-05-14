abc87ba740dd98da02a0289da93da1bb72164f6f
Matthew Carter <m@ahungry.com>
2019-05-14 00:45:58 -0400

    Add simple gui sample

---
+++ item-shop/gui/src/item-shop/core.clj
@@ -0,0 +1,16 @@
+(ns item-shop.core
+  (:require [cljfx.api :as fx]))
+
+(defn -main [& args]
+  (fx/on-fx-thread
+   (fx/create-component
+    {:fx/type :stage
+     :showing true
+     :title "Cljfx example"
+     :width 300
+     :height 100
+     :scene {:fx/type :scene
+             :root {:fx/type :v-box
+                    :alignment :center
+                    :children [{:fx/type :label
+                                :text "Hello world"}]}}})))
